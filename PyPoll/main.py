60f12511af22008ac04387472c94f919d93cb7a0
tc-sabr <42155277+tc-sabr@users.noreply.github.com>
2018-09-11 12:56:06 -0600

    add code to read txt file on terminal and commit txt file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -33,24 +33,46 @@ with open(csvpath, newline='') as csvfile:
 
 candidate_votes = Counter(candidates)
 
+# #print header to terminal
+# print("Election Results")
+# print("---------------------------------")
+
+# #print total votes to terminal
+# print(f"Total Votes: {total_votes}")
+# print("---------------------------------")
+
+# #print the candidate names, percent won and their vote total to terminal
+# for key, value in candidate_votes.items():
+#     percent = round((value / total_votes) * 100, 2)
+#     print(f"{key} {percent}% ({value})")
+
+# print("---------------------------------")
+
+# #print winner to terminal
+# print(f"Winner: {max(candidate_votes, key=candidate_votes.get)}")
+
+# print("---------------------------------")
+
 with open("PyPoll_Results.txt", "w") as text_file:
 
-    #print header
+    #print header to file
     print("Election Results", file=text_file)
     print("---------------------------------", file=text_file)
 
-    #print total votes
+    #print total votes to file
     print(f"Total Votes: {total_votes}", file=text_file)
     print("---------------------------------", file=text_file)
 
-    #print the candidate names, percent won and their vote total
+    #print the candidate names, percent won and their vote total to file
     for key, value in candidate_votes.items():
         percent = round((value / total_votes) * 100, 2)
         print(f"{key} {percent}% ({value})", file=text_file)
 
     print("---------------------------------", file=text_file)
 
-    #print winner
+    #print winner to file
     print(f"Winner: {max(candidate_votes, key=candidate_votes.get)}", file=text_file)
 
     print("---------------------------------", file=text_file)
+    
+print(open('PyPoll_Results.txt').read())
